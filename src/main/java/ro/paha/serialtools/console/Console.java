ebb34d9e79489b54a6450e2102e9123fff907fd1
Gavrila Florin <gavrilacflorin@yahoo.com>
2020-04-30 02:56:12 +0300

    switched to spring shell

--- src/main/java/ro/paha/serialtools/console/Console.java
+++ src/main/java/ro/paha/serialtools/console/Console.java
@@ -1,15 +1,26 @@
 package ro.paha.serialtools.console;
 
-import com.beust.jcommander.JCommander;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import ro.paha.serialtools.Connector;
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.shell.jline.PromptProvider;
+
+@SpringBootApplication(exclude = {JmxAutoConfiguration.class})
 public class Console {
-    public static void main(String[] args) {
-        CommandLineArgs arguments = new CommandLineArgs();
-        JCommander.newBuilder()
-                .addObject(arguments)
-                .build()
-                .parse(args);
+    private static Connector scm;
 
-        System.out.println(arguments.getVerbose().intValue());
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext context = SpringApplication.run(Console.class, args);
+    }
+
+    @Bean
+    public PromptProvider myPromptProvider() {
+        return () -> new AttributedString("serial-tools:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
     }
 }
